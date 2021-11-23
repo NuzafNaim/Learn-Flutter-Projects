06bec76429df1f98ee969b65d645babfc16b15e2
K M Nuzaf Naim Saikat <nuzafnaim@gmail.com>
2021-11-24 03:42:50 +0600

    Button Added

--- dicee_app/lib/main.dart
+++ dicee_app/lib/main.dart
@@ -27,14 +27,22 @@ class DiceePage extends StatelessWidget {
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('assets/images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    print('Its Pressed');
+                  },
+                  child: Image.asset('assets/images/dice1.png')),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('assets/images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    print('Pressed');
+                  },
+                  child: Image.asset('assets/images/dice1.png')),
             ),
           ),
         ],
