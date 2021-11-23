61dcb3d26f6f253863fab1d5846a33536c81bd12
K M Nuzaf Naim Saikat <nuzafnaim@gmail.com>
2021-11-24 03:26:54 +0600

    UI Ready

--- dicee_app/lib/main.dart
+++ dicee_app/lib/main.dart
@@ -22,15 +22,23 @@ class DiceePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('assets/images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('assets/images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('assets/images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('assets/images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
