42b72b385572dd1dc83840bbe10a6579dd5d8fe8
K M Nuzaf Naim Saikat <nuzafnaim@gmail.com>
2021-11-23 06:19:23 +0600

    My Card App Complete

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -12,7 +12,78 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('assets/images/saikat.jpg'),
+              ),
+              Text(
+                'Saikat Khandoker',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 35.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SSP',
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  thickness: 2,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+88 01878032290',
+                    style: TextStyle(
+                      fontFamily: 'SSP',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'hello@nuzafnaim.dev',
+                    style: TextStyle(
+                      fontFamily: 'SSP',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
